a8e9144b26e242f2212393eaa47eb738acb9850d
Xingyunguan <52118660+Xingyunguan@users.noreply.github.com>
2023-08-27 21:23:43 +0800

    Update notification.py

--- plyer/platforms/android/notification.py
+++ plyer/platforms/android/notification.py
@@ -9,7 +9,7 @@ AndroidString = autoclass('java.lang.String')
 Context = autoclass('android.content.Context')
 NotificationBuilder = autoclass('android.app.Notification$Builder')
 NotificationManager = autoclass('android.app.NotificationManager')
-#Drawable = autoclass("{}.R$drawable".format(activity.getPackageName()))
+Drawable = autoclass("{}.R$drawable".format(activity.getPackageName()))
 PendingIntent = autoclass('android.app.PendingIntent')
 Intent = autoclass('android.content.Intent')
 Toast = autoclass('android.widget.Toast')
@@ -24,17 +24,8 @@ class AndroidNotification(Notification):
     '''
 
     def __init__(self):
-        package_name = activity.getPackageName()
         self._ns = None
-        self._channel_id = package_name
-
-        pm = activity.getPackageManager()
-        info = pm.getActivityInfo(activity.getComponentName(), 0)
-        if info.icon == 0:
-            # Take the application icon instead.
-            info = pm.getApplicationInfo(package_name, 0)
-
-        self._app_icon = info.icon
+        self._channel_id = None
 
     def _get_notification_service(self):
         if not self._ns:
@@ -91,10 +82,12 @@ class AndroidNotification(Notification):
 
         .. versionadded:: 1.4.0
         '''
-        app_icon = self._app_icon
+
+        app_icon = Drawable.your_icon_name_without_extensionname
         notification.setSmallIcon(app_icon)
 
         bitmap_icon = app_icon
+
         if icon is not None:
             bitmap_icon = BitmapFactory.decodeFile(icon)
             notification.setLargeIcon(bitmap_icon)
